376e153021b35c664e70363f2ae44928fa55bc34
David Huculak <davidhuculak5@gmail.com>
2024-07-13 15:41:25 -0400

    compiling

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,34 @@ use half::f16;
 pub struct EncodedUnitVector([f16; 2]);
 
 impl EncodedUnitVector {
-    pub fn from_array_f32(mut unit_vector: [f32; 3]) {
+    pub fn from_array(unit_vector: [f32; 3]) -> Self {
         // TODO: add debug assertion that it is normalized
-        n = n / (n.x.abs() + n.y.abs() + n.z.abs());
-        let mut result_vec2 = if n.z >= 0.0 { n.xy() } else { oct_wrap(n.xy()) };
-        result_vec2 = result_vec2 * Vec2::splat(0.5) + Vec2::splat(0.5);
-        [f16::from(result_vec2.x), f16::from(result_vec2.y)]
+        // TODO: accelerate with SIMD?
+        let mut n = unit_vector;
+        let sum = n[0].abs() + n[1].abs() + n[2].abs();
+        n[0] /= sum;
+        n[1] /= sum;
+        n[2] /= sum;
+        let mut result_vec2 = if n[2] >= 0.0 {
+            [n[0], n[1]]
+        } else {
+            oct_wrap([n[0], n[1]])
+        };
+        result_vec2[0] = result_vec2[0] * 0.5 + 0.5;
+        result_vec2[1] = result_vec2[1] * 0.5 + 0.5;
+        Self([f16::from_f32(result_vec2[0]), f16::from_f32(result_vec2[1])])
     }
+
+    pub fn raw(&self) -> [f16; 2] {
+        self.0
+    }
+}
+
+fn oct_wrap(v: [f32; 2]) -> [f32; 2] {
+    [
+        if v[0] >= 0.0 { 1.0 } else { -1.0 } * (1.0 - v[1].abs()),
+        if v[1] >= 0.0 { 1.0 } else { -1.0 } * (1.0 - v[0].abs()),
+    ]
 }
 
 pub fn add(left: usize, right: usize) -> usize {
